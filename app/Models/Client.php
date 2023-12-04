69d3f89fd2fa8d938fae5ea8330310718b09219e
RenanRodriguesTI <renanrodriguesmachado2@gmail.com>
2023-12-03 22:18:56 -0300

    feat:  aula migrate e class

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address_id',
+        'user_id'
+    ];
+
+    public function address(): BelongsTo{
+        return $this->belongsTo(Address::class);
+    }
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function sales(): HasMany{
+        return $this->hasMany(Sale::class);
+    }
 }
--- app/Models/company.php
+++ app/Models/company.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class company extends Model
 {
     use HasFactory;
+
+    protected $fillabel = [
+        'commission_rate',
+        'name'
+    ];
 }
